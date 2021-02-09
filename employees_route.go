865924c889a43d84b99e1558223a88e235964b9c
Ramli <75848649+bams32@users.noreply.github.com>
2021-02-10 01:11:25 +0700

    Add files via upload

---
+++ employees_route.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"Tugas7/echo-rest/controllers"
+	"github.com/labstack/echo"
+)
+
+// EmployeesRoute ...
+func EmployeesRoute(g *echo.Group) {
+	g.POST("/list", controllers.FetchAllEmployees)
+	g.POST("/add", controllers.StoreEmployee)
+	g.POST("/update", controllers.UpdateEmployee)
+	g.POST("/delete", controllers.DeleteEmployee)
+}
---
+++ suppliers_route.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"Tugas7/echo-rest/controllers"
+	"github.com/labstack/echo"
+)
+
+// SuppliersRoute ...
+func SuppliersRoute(g *echo.Group) {
+	g.POST("/list", controllers.FetchAllSuppliers)
+	g.POST("/add", controllers.StoreSupplier)
+	g.POST("/update", controllers.UpdateSupplier)
+	g.POST("/delete", controllers.DeleteSupplier)
+}
